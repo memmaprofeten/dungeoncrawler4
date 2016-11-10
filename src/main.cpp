ec0c1de914ae8072b49caf7acc4b0cadf775bdea
Joel Sågfors <sagforj1@aalto.fi>
2016-11-10 20:56:34 +0200

    Made a controllable triangle pointing to mouse poisition

---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include <SFML/Graphics.hpp>
+#include <cmath>
+int main()
+{
+   	sf::RenderWindow window(sf::VideoMode(800, 600), "The game!");
+    	sf::CircleShape shape(20,3);
+	shape.setOrigin(20,20);
+    	shape.setFillColor(sf::Color::Red);
+	const float PI = 3.14159265;
+    	while (window.isOpen())
+    	{
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
+		shape.move(1,0);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
+		shape.move(-1,0);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
+		shape.move(0,1);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
+		shape.move(0,-1);
+	}
+	sf::Vector2f shapepos = shape.getPosition();
+	sf::Vector2i mousepos = sf::Mouse::getPosition(window);
+	float dx = shapepos.x - mousepos.x;
+	float dy = shapepos.y - mousepos.y;
+	float rotation = (atan2(dy,dx)) * 180 / PI;
+	shape.setRotation(rotation+30);
+    }
+
+    return 0;
+}
