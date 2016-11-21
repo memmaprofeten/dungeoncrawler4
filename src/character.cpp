90abdcd84273e47c5929b70ed338863787b1ef40
jetten <johan.rabb@aalto.fi>
2016-11-21 16:36:05 +0200

    Basic implementation of Character class

---
+++ src/character.cpp
@@ -0,0 +1,7 @@
+#include "character.hpp"
+
+Character::Character(const std::string& n, bool t, int l) : name(n), type(t), level(l) {
+	
+	
+	
+}
--- src/character.hpp
+++ src/character.hpp
@@ -1,38 +1,44 @@
-#include "weapon.hpp"
-#include "item.hpp"
+#ifndef CHARACTER_HH
+#define CHARACTER_HH
+
 #include <string>
-#include <list>
+#include <vector>
+#include <SFML/Graphics.hpp>
+//#include "weapon.hpp"
+#include "item.hpp"
 
 /*Can it be made so that only one instance of character can
 be created?*/
 class Character {
 public:
-/*Constructor taking name, type and level with default value 1
-This can be used to initiate higher level characters if we decide
-to implement a save feature
+	/*Constructor taking name, type and level with default value 1
+	This can be used to initiate higher level characters if we decide
+	to implement a save feature */
+	Character(const std::string& n, bool t, int l = 1);
 
-moving function
 
-attacking and getting attacked functions
+	std::string getName() const {return name;}
 
-item pickup*/
+	// moving function
+
+	//attacking and getting attacked functions
+
+	//item pickup
+
 private:
-/*
-name
-type
-level
-list of weapons
-list of other items
-
-Basic variables. Possibly forgetting something though.*/
-  std::string name; //Name of player. User input or based on class?
-  int level; //Character level of player. Starts at 1
-  std::list<Weapon> weapons; //List of weapons carried by player.
-  std::list<Item> items; //List of items carried by player.
-  Weapon equippedweapon; //currently equipped weapon.
-  int maxhealth; //Player's maximum health.
-  int health; //Player's current health.
-  float movespeed; //Players movement speed.
-  int xp; //Player's current experience amount.
-  int xpfornextlevel; //amount required for player to level up.
+	sf::Sprite sprite;
+	std::string name; //Name of player. User input or based on class?
+	bool type;
+	int level; //Character level of player. Starts at 1
+	//std::vector<Weapon> weapons; //List of weapons carried by player.
+	std::vector<Item> items; //List of items carried by player.
+	
+	//Weapon equippedweapon; //currently equipped weapon.
+	int maxhealth; //Player's maximum health.
+	int health; //Player's current health.
+	float movespeed; //Players movement speed.
+	int xp; //Player's current experience amount.
+	int xpfornextlevel; //amount required for player to level up.
 };
+
+#endif
