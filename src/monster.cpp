d3187b106bfd3d5938c1fab27bdc1217a507e276
Matikainen Antti <matikaa2@sitriini.org.aalto.fi>
2016-11-21 14:19:49 +0200

    Included basic functions and header junk for monster class. Also included some functions that have yet to be implemented. Comments will explain what needs further work.

---
+++ src/monster.cpp
@@ -0,0 +1,123 @@
+#include <monster.hpp>
+#include <weapon.hpp>
+#include <item.hpp>
+/*
+Basic functions. There's probably some brainfarting going on since I have the memory of a goldfish.
+
+Todo: Aggrocheck. Pathfinding. General AI. Collision for movement. 
+
+Collision requires some way to tie xy position to specific tile on map.
+Pathfinding requires some shenaniganry and/or googling the algorithms.
+Aggrocheck requires way to get position of player.
+General AI requires above things to be complete.
+ */
+const std::string returnname(){
+  return name;
+}
+const int Monster::returnhealth(){
+  return health;
+}
+
+const int Monster::returnxponkill(){
+  return xponkill;
+}
+
+const int Monster::returnattackdamage(){
+  return attackdamage;
+}
+
+const float Monster::returnmovespeed(){
+  return movespeed;
+}
+
+const Weapon Monster::returncarriedweapon(){
+  return carriedweapon;
+}
+
+const Item Monster::returncarrieditem(){
+  return carrieditem;
+}
+
+const int Monster::returnaggrorange(){
+  return aggrorange;
+}
+
+const int Monster::returnxpos(){
+  return xpos;
+}
+
+const int Monster::returnypos(){
+  return ypos;
+}
+
+//Set x and y coordinates of monster.
+void Monster::setxypos(int x, int y){
+  xpos = x;
+  ypos = y;
+}
+
+//Change x and y coordinates by input amount. Direction handled by negative input value.
+void Monster::changexypos(int xchange, int ychange){
+  xpos += xchange;
+  ypos += ychange;
+}
+
+/*
+xdir, 1 if moving right, -1 if moving left. 
+ydir, 1 if moving up, -1 if moving down. 
+0 for either if not moving along that axis. 
+Basically iterates x and y positions based on movespeed in directions given by inputs. 
+Should enable movement in 8 directions.
+Does not handle collision, as I have no idea how.
+*/ 
+void Monster::monstermove(int xdir, int ydir){
+  changexypos(std::floor(xdir*movespeed), std::floor(ydir*movespeed));
+}
+
+//Returns true if player is within range of monster defined by aggrorange variable. Will be done once the player character class is more defined.
+bool Monster::monsteraggrocheck(Character player){
+}
+
+//Reduces health by given amount. Then returns health so function that calls it can check if the monster died.
+int Monster::reducehealth(int reducedby){
+  health -= reducedby;
+  return health;
+}
+
+// Constructors for melee and ranged monster classes.
+void RangedMonster::RangedMonster(std::string namei, int healthi, int xponkilli, int attackdamagei, float movespeedi, Item carreditemi, Weapon carriedweaponi, int aggrorangei, float projectilespeedi, float projectilerangei){
+  name = namei
+  health = healthi;
+  xponkill = xponkilli;
+  attackdamage = attackdamagei;
+  movespeed = movespeedi;
+  carrieditem = carrieditemi;
+  carriedweapon = carriedweaponi;
+  aggrorange = aggrorangei;
+  projectilespeed = projectilespeedi;
+  projectilerange = projectilerangei;
+}
+
+void MeleeMonster::MeleeMonster(std::string namei, int healthi, int xponkilli, int attackdamagei, float movespeedi, Item carrieditemi, Weapon carriedweaponi, int aggrorangei, int attackrangei, int attackarci){
+  name = namei
+  health = healthi;
+  xponkill = xponkilli;
+  attackdamage = attackdamagei;
+  movespeed = movespeedi;
+  carrieditem = carrieditemi;
+  carriedweapon = carriedweaponi;
+  aggrorange = aggrorangei;
+  attackrange = attackrangei;
+  attackarc = attackarci
+}
+
+/*
+Functions for attacking. Melee monster attacks check if the player is within range of the enemy. If they are, it reduces player damage based on its attackdamage variable. 
+
+Ranged enemies will, if the player is within their attack range, fire a projectile at them. Not entirely sure how to handle said projectile.
+
+Potential issues: Will attack every time function is called. Requires whatever AI function is calling it to limit how often the monster can attack.
+*/
+void Rangedmonster::monsterattack(int playerxpos, int playerypos){}
+
+void MeleeMonster::monsterattack(Character player){}
--- src/monster.hpp
+++ src/monster.hpp
@@ -12,28 +12,48 @@ Moving function which is triggerd when character is close enough
 or maybe always running around at random
 */
 
-  const bool returntype();
+/*
+General "return value" functions.
+*/
+  const std::string returnname();
   const int returnhealth();
   const int returnxponkill();
   const int returnattackdamage();
-  const int returnattackrange();
-  const float returnprojectilespeed();
   const float returnmovespeed();
   const Weapon returncarriedweapon();
   const Item returncarrieditem();
   const int returnaggrorange();
+  const int returnxpos();
+  const int returnypos();
+  void setxypos(int x, int y);
+  void changexypos(int xchange, int ychange);
+
+/*
+Reduce monster health. To be called by whatever handles the monster getting hit.
+*/
+  
+  int reducehealth(int reducedby);
+
+/*
+Functions to be called by the AI code. In theory, handle movement and attacking.
+Monster attack function virtual so ranged and melee monsters attack differently.
+*/
+
+  virtual void monsterattack() = 0; //Handles attack by the monster.
+  void monstermove(int xdir, int ydir); //Handles movement.
+  bool monsteraggrocheck(Character player);//Checks if player is within range of monster.
  
 private:
-  bool type; //True = Ranged, False = Melee. Mahdollisesti toteutetaan käyttämällä eri luokkia?
+  std::string name; //Enemy name.
   int health; //Enemy health.
   int xponkill; //Experience given to player when killed.
   int attackdamage; //Damage dealt on attack.
-  int attackrange; //Attack range for melee enemies.
-  float projectilespeed; //Speed of projectile for ranged enemies.
   float movespeed; //Movementspeed of enemy.
   Item carrieditem; //Item to be carried. Dropped on death.
   Weapon carriedweapon; //Weapon carried. Dropped on death.
   int aggrorange; //AI Parameter. How close the player will have to be for the monster to move towards them.
+  int xpos;
+  int ypos;
 /*
 type
 health
@@ -42,3 +62,26 @@ weapon
 item (to be picked up by character after death
 isAlive)
 */};
+
+class RangedMonster : public Monster {
+public:
+  void monsterattack(int playerxpos, int playerypos);
+  RangedMonster(std::string namei, int healthi, int xponkilli, int attackdamagei, float movespeedi, Item carreditemi, Weapon carriedweaponi, int aggrorangei, float projectilespeedi, float projectilerangei);
+  
+private:
+  float projectilespeed; //How fast the projectile fired by the enemy moves.
+  float projectilerange; //How far the projectile travels / is active.
+}
+
+class MeleeMonster : public Monster{
+public:
+  void monsterattack(Character player);
+  MeleeMonster(std::string namei, int healthi, int xponkilli, int attackdamagei, float movespeedi, Item carrieditemi, Weapon carriedweaponi, int aggrorangei, int attackrangei, int attackarci);
+
+private:
+  int attackrange; //Radius of the enemy's attack swing
+  int attackarc; //How wide the swing is.
+
+  /*Essentially, the way I thought of the monsters attack is as a semicircle with an arc and a radius. If the player is within that radius, they'll get hit. Should also hepl with graphics, depending on how we choose to represent an enemy's attack. */
+
+}
