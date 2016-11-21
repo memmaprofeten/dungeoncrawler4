975fda81f4447d7c314d0965d9d919c75413794f
Joel Sågfors <sagforj1@aalto.fi>
2016-11-21 12:02:50 +0200

    Created proper headers for the map

--- src/map.hpp
+++ src/map.hpp
@@ -5,5 +5,10 @@ public:
 /*Constructor which picks at random from a list of 
 pre-created rooms*/
 /*Function to draw current room in the game screen*/
+	Map(int numberOfRooms);
 private:
-/*array pointers/references to rooms stored in some kind of list structure*/};
+
+/*array pointers/references to rooms stored in some kind of list structure*/
+	std::vector<Room>& map;
+	int atRoom;
+};
