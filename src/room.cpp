511f681f7c30448d8705e36a8005f917c35ed0c5
Berthur <jon.galan@aalto.fi>
2016-11-21 15:32:34 +0200

    Convenience functions in class Room. Testing.

--- src/room.cpp
+++ src/room.cpp
@@ -1,6 +1,4 @@
 #include "room.hpp"
-#include <string>
-#include <iostream>
 
 Room::Room(std::string const file) {
     (void)file; // TODO: Parse and load file
@@ -25,9 +23,33 @@ Room::Room(std::string const file) {
     height = 3;
 }
 
+bool Room::hasCoordinate(int x, int y) {
+    if (x < 0 || y < 0 || x >= width || y >= height) {
+        return false;
+    }
+    return true;
+}
+
 Tile& Room::getTile(int x, int y) {
-    // TODO: Check bounds
-    return room[x][y];
+    if (hasCoordinate(x, y)) {
+        return room[x][y];
+    }
+    throw std::runtime_error("The coordinates are out of bounds!");
+}
+
+std::vector<sf::Vector2i> Room::getNeighbours(int x, int y, bool includingSelf, bool includingDiagonals, bool includingOutsiders) {
+    std::vector<sf::Vector2i> res;
+    for (int dx = -1; dx <= 1; ++dx) {
+        for (int dy = -1; dy <= 1; ++dy) {
+            if ((includingSelf || !(dx == 0 && dy == 0)) &&          // Check if this neighbour is the original coordinate
+                (includingDiagonals || dx == 0 || dy == 0) &&       // Check if this neighbour is a diagonal neighbour
+                (includingOutsiders || (dx >= 0 && dy >= 0 && dx < width && dy < height))   // Check if this neighbour is out of bounds
+            ) {
+                res.push_back(sf::Vector2i(x + dx, y + dy));
+            }
+        }
+    }
+    return res;
 }
 
 void Room::print() {
