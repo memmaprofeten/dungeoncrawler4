a977d6eb8449fc00d4c0b79c6f12e8585b33929d
Berthur <jon.galan@aalto.fi>
2016-12-12 23:57:47 +0200

    Simple animation logic

--- src/settings.hpp
+++ src/settings.hpp
@@ -39,6 +39,24 @@ namespace s {
 	extern std::vector<std::string> soundfilepaths;
 	extern std::vector<sf::SoundBuffer> soundbuffers;
 	void loadsounds();
+
+	/* Represents a simple translation/rotation animation.
+	*/
+	struct animation {
+		sf::Vector2f rPos;		// The rest position of the animation
+		sf::Vector2f pos;		// The current position of the animation
+		sf::Vector2f dPos;		// The relative translation
+		float rRot;				// The rest rotation of the animation
+		float rot;				// The current roation of the animation
+		float dRot;				// The relative rotation
+		float duration;			// The duration of the rotation
+		float accumulator;		// An accumulator keeping track of the lifetime of the current animation round
+		bool active;			// A flag indicating whether or not this animation is currently in progress.
+		sf::Sprite sprite;		// The sprite of the animation
+		void set(sf::Vector2f p, sf::Vector2f dp, float r, float dr, int textureIndex, sf::Vector2f scale, sf::Vector2f origin, float dur);
+		void restart();
+		void draw(sf::RenderWindow& window, float elapsed);
+	};
 }
 
 #endif
